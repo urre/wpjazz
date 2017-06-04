c2b9d0d3f47b86a49958b6dcb2dcc5155afd3169
Urban Sanden <hej@urre.me>
2017-06-04 13:13:08 +0200

    Production build tweak

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -15,6 +15,9 @@ module.exports = {
 		publicPath: '/',
 	},
 	plugins: [
+	new webpack.DefinePlugin({
+	  'process.env.NODE_ENV': JSON.stringify('production')
+	}),
 		new webpack.optimize.UglifyJsPlugin({
 			compressor: {
 				warnings: false
